1464d4e2fd96cefc0d837bccbbe4512a61f548aa
MichaelAlanAXL <michael.dtna@gmail.com>
2025-09-21 23:47:18 -0300

    feat: estrutura básica do projeto iniciada com Slim 4 e Twig

--- public/index.php
+++ public/index.php
@@ -2,23 +2,19 @@
 require __DIR__ . '/../vendor/autoload.php';
 
 use Slim\Factory\AppFactory;
-
-// configurações do RainTPL
-\Rain\Tpl::configure([
-    "tpl_dir"   => __DIR__ . "/../templates/",
-    "cache_dir" => __DIR__ . "/../cache/",
-    "debug"     => true
-]);
+use Slim\Views\Twig;
+use Slim\Views\TwigMiddleware;
+use App\Controllers\Site\HomeController;
 
 $app = AppFactory::create();
 
-// rota de teste
-$app->get('/', function($request, $response) {
-    $tpl = new \Rain\Tpl();
-    $tpl->assign("name", "Mundo");
-    $html = $tpl->draw("home", true);
-    $response->getBody()->write($html);
-    return $response;
-});
+$twig = Twig::create(__DIR__ . '/../templates', ['cache' => false]);
+$app->add(TwigMiddleware::create($app, $twig));
+
+$homeController = new HomeController($twig);
+
+$app->get('/', [$homeController, 'index']);
+
+$app->get('/login', [$homeController, 'login']);
 
 $app->run();
---
+++ src/Controllers/Site/HomeController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Controllers\Site;
+
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Slim\Views\Twig;
+
+class HomeController 
+{
+    private Twig $twig;
+
+    public function __construct(Twig $twig) 
+    {
+        $this->twig = $twig;
+    }    
+
+    private function render(Response $response, string $template, array $data = [], bool $withLayout = true): Response 
+    {
+        if ($withLayout) {
+            $response = $this->twig->render($response, 'header.html.twig', $data);
+        }
+
+        $response = $this->twig->render($response, $template . '.html.twig', $data);
+
+        if ($withLayout) {
+            $response = $this->twig->render($response, 'footer.html.twig', $data);
+        }
+
+        return $response;
+    }
+
+    public function index(Request $request, Response $response, $args): Response
+    {
+        return $this->render($response, "site/home", [
+            'title' => 'Home',
+            'username' => 'Michael',
+            'cartCount' => 0
+        ]);
+    }
+
+    public function login(Request $request, Response $response, $args): Response
+    {
+        return $this->render($response, "site/login", [
+            "title" => "Login"
+        ], false); // without header/footer
+    }
+}
